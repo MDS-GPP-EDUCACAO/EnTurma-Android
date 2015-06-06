3caf73e6c573ad0bf5b3f8266ee7bc07e519f895
Gabriel Araújo <gabrieldea.silva@gmail.com>
2015-06-06 12:11:43 -0300

    Adding Compare Reports Fragment

--- EnTurma/app/src/main/java/br/com/projetoenturma/enturma/CompareFragment.java
+++ EnTurma/app/src/main/java/br/com/projetoenturma/enturma/CompareFragment.java
@@ -1,13 +1,24 @@
 package br.com.projetoenturma.enturma;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class CompareFragment extends Fragment {
+
+
+    Spinner firstYearSpinner, firstStateSpinner, firstGradeSpinner, firstNetworkSpinner,firstPublicTypeSpinner, firstLocalSpinner;
+    Spinner secondYearSpinner, secondStateSpinner, secondGradeSpinner, secondNetworkSpinner,secondPublicTypeSpinner, secondLocalSpinner;
+    Button requestButton;
+    ProgressDialog activityIdicator;
+
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -43,4 +54,64 @@ public class CompareFragment extends Fragment {
                 getArguments().getInt(ARG_SECTION_NUMBER));
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        firstYearSpinner = (Spinner) getView().findViewById(R.id.first_year);
+        firstStateSpinner = (Spinner) getView().findViewById(R.id.first_state);
+        firstGradeSpinner = (Spinner) getView().findViewById(R.id.first_grade);
+        firstNetworkSpinner = (Spinner) getView().findViewById(R.id.first_network);
+        firstPublicTypeSpinner = (Spinner) getView().findViewById(R.id.first_public_type);
+        firstLocalSpinner = (Spinner) getView().findViewById(R.id.first_local);
+
+        secondYearSpinner = (Spinner) getView().findViewById(R.id.second_year);
+        secondStateSpinner = (Spinner) getView().findViewById(R.id.second_state);
+        secondGradeSpinner = (Spinner) getView().findViewById(R.id.second_grade);
+        secondNetworkSpinner = (Spinner) getView().findViewById(R.id.second_network);
+        secondPublicTypeSpinner = (Spinner) getView().findViewById(R.id.second_public_type);
+        secondLocalSpinner = (Spinner) getView().findViewById(R.id.second_local);
+
+
+        requestButton = (Button) getView().findViewById(R.id.send_report);
+
+        setupActions();
+
+        activityIdicator = new ProgressDialog(getActivity());
+        activityIdicator.setMessage("Procurando pelo relatório");
+        activityIdicator.setCancelable(false);
+    }
+
+    private void setupActions(){
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                requestData();
+            }
+        });
+
+        showWhenEqual("Publica", firstPublicTypeSpinner, firstNetworkSpinner);
+        showWhenEqual("Publica", secondPublicTypeSpinner, secondNetworkSpinner);
+    }
+
+    private void showWhenEqual(final String value, final Spinner show, final Spinner entry){
+        entry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (entry.getSelectedItem().toString().equals(value)) {
+                    show.setVisibility(View.VISIBLE);
+                } else {
+                    show.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void requestData(){
+        //request data ans plot graphs
+    }
 }
