f16a8c0f4c480da2b05e9e3565f97292cdc7f055
PedroSales <pedrosal3s@gmail.com>
2015-06-23 14:50:55 -0300

    Improving Ranking row layout
    Signed-off-by: PedroSales <pedrosal3s@gmail.com>
    Signed-off-by: Thiago Bernardes <tmb0710@gmail.com>

--- EnTurma/app/src/main/java/customAdapters/RankingAdapter.java
+++ EnTurma/app/src/main/java/customAdapters/RankingAdapter.java
@@ -1,6 +1,8 @@
 package customAdapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,7 +61,14 @@ public class RankingAdapter extends BaseAdapter {
         TextView state = (TextView) cell.findViewById(R.id.state);
         TextView score = (TextView) cell.findViewById(R.id.score);
 
-        rankingPosition.setText(position+1 + "°");
+        rankingPosition.setText(position + 1 + "°");
+
+        if(position == 0){
+            rankingPosition.setTextColor(Color.rgb(255, 204 , 102 ));
+            state.setTextColor(Color.rgb(255, 204, 102));
+            score.setTextColor(Color.rgb(255, 204, 102));
+        }
+
 
         Map<String,String> currentCellData = data.get(position);
 
